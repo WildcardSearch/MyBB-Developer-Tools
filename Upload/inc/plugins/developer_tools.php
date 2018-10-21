f657099fbdd426e37430177939c4547223f1aba4
Mark Vincent <admin@rantcentralforums.com>
2018-10-21 14:06:25 -0500

    0.1 Pre-release

--- Upload/inc/plugins/developer_tools.php
+++ Upload/inc/plugins/developer_tools.php
@@ -12,7 +12,7 @@ if (!defined('IN_MYBB')) {
     die('Direct initialization of this file is not allowed.<br /><br />Please make sure IN_MYBB is defined.');
 }
 
-define('DEVELOPER_TOOLS_VERSION', '0.0.4');
+define('DEVELOPER_TOOLS_VERSION', '0.1');
 define('DEVELOPER_TOOLS_MOD_URL', MYBB_ROOT . 'inc/plugins/developer_tools/modules');
 
 // register custom class autoloader
